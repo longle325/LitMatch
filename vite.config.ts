f0121b3ff47258b6ddc694eb039408abb58fec6e
trietdeptrai <triet2one@gmail.com>
2026-05-09 10:28:00 +0700

    Improve literary chat experience

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "node:path";
+
+const srcPath = decodeURIComponent(new URL("./src", import.meta.url).pathname);
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      "@": srcPath,
     },
   },
   server: {
